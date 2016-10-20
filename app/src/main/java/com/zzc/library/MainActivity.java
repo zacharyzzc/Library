5afaeaaf2bce1de23d7e1b7f3c0ebae673eac14a
Zachary <563437071@qq.com>
2016-10-20 14:33:11 +0800

    init

---
+++ app/src/main/java/com/zzc/library/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zzc.library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("111","222");
+    }
+}
